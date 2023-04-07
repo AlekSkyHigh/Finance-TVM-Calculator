4b08a68a681a9defe7778e9389b309229535d27b
AlekSkyHigh <aleksandar.voynov.info@gmail.com>
2023-04-07 15:05:02 +0300

    Added Quarterly payment period option

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function calculateFutureValue(presentValue, payments, interestRate, periods) {
 
     const compundingChoice = document.getElementById('select-choice');
 
-    if (compundingChoice.value === "Annualy") {
+    if (compundingChoice.value == "Annualy") {
 
         let futureValue = -1 * presentValue;
         for (let i = 1; i <= periods; i++) {
@@ -11,21 +11,30 @@ function calculateFutureValue(presentValue, payments, interestRate, periods) {
         }
         return futureValue.toFixed(2);
 
-    } else if (compundingChoice.value === "Monthly") {
+    } else if (compundingChoice.value == "Monthly") {
 
-        const monthlyInterestRate = (interestRate / 12) / 100;
-        const futureValue = Math.abs(presentValue) * Math.pow(1 + monthlyInterestRate, periods) -
-            payments * ((Math.pow(1 + monthlyInterestRate, periods) - 1) / monthlyInterestRate) * (1 + monthlyInterestRate);
+        const monthlyRate = (interestRate / 12) / 100;
+        const futureValue = Math.abs(presentValue) * Math.pow(1 + monthlyRate, periods) -
+            payments * ((Math.pow(1 + monthlyRate, periods) - 1) / monthlyRate) * (1 + monthlyRate);
 
         return futureValue.toFixed(2);
 
-    } else if (compundingChoice.value === "Semiannually") {
+    } else if (compundingChoice.value == "Semiannually") {
 
         const semiAnnualRate = (interestRate / 2) / 100;
         const futureValue = Math.abs(presentValue) * Math.pow(1 + semiAnnualRate, periods) - 
             payments * ((Math.pow(1 + semiAnnualRate, periods) - 1) / semiAnnualRate) * (1 + semiAnnualRate);
 
-        return futureValue.toFixed(2)
+        return futureValue.toFixed(2);
+
+    } else if (compundingChoice.value == "Quarterly"){
+
+        const quarterlyRate = (interestRate / 4) / 100;
+        const futureValue = Math.abs(presentValue) * Math.pow(1 + quarterlyRate, periods) - 
+            payments * ((Math.pow(1 + quarterlyRate, periods) - 1) / quarterlyRate) * (1 + quarterlyRate);
+
+        return futureValue.toFixed(2);
+
     }
 
 }
@@ -46,5 +55,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
         futureValueInputField.value = futureValue
     });
-    
+
 });
