3e66b3edd8d0199cd10a0eca8071ef1d1f70d64e
AlekSkyHigh <aleksandar.voynov.info@gmail.com>
2023-04-10 13:44:56 +0300

    added new features

--- functionalities/periods.js
+++ functionalities/periods.js
@@ -1,16 +1,16 @@
-// export function calculatePeriods(presentValue, payment, futureValue, rate) {
-//     // Convert annual rate from percent to decimal
-//     rate = rate / 100;
+export function calculatePeriods(presentValue, payment, futureValue, rate) {
+    // Convert annual rate from percent to decimal
+    rate = rate / 100;
   
-//     // Calculate the number of periods required to achieve the future value
-//     const periods = Math.log((futureValue - payment) / (presentValue - payment)) / Math.log(1 + rate);
+    // Calculate the number of periods required to achieve the future value
+    const periods = Math.log((futureValue - payment) / (presentValue - payment)) / Math.log(1 + rate);
   
-//     // Convert periods to years
-//     const years = periods / 1;
+    // Convert periods to years
+    const years = periods / 1;
   
-//     // Return the number of years required
-//     return years;
-//   }
+    // Return the number of years required
+    return years;
+  }
   
 
 
--- functionalities/rate.js
+++ functionalities/rate.js
@@ -1,35 +1,81 @@
-export function calculateRate(presentValue, annualPayment, futureValue, years) {
-    const n = years; // number of years
-    const p = -presentValue; // present value (negative to represent investment)
-    const r = 0.01; // starting guess for annual interest rate
-    const f = futureValue; // future value
-    let guess = r;
-    let tolerance = 0.0001;
+export function calculateRate(presentValue, payments, futureValue, years) {
 
-    function calculateFutureValue(rate) {
-        const compoundedValue = p * Math.pow((1 + rate), n); // compounded value of present value
-        const annuityValue = -annualPayment * ((Math.pow((1 + rate), n) - 1) / rate); // annuity value of annual payment (negative to represent investment)
-        return compoundedValue + annuityValue + f; // future value
+    const compundingChoice = document.getElementById('select-choice');
+
+    if (compundingChoice.value == "Annualy") {
+
+        const n = years; // number of years
+        const p = -presentValue; // present value (negative to represent investment)
+        const r = 0.01; // starting guess for annual interest rate
+        const f = futureValue; // future value
+        let guess = r;
+        let tolerance = 0.0001;
+
+        function calculateFutureValue(rate) {
+            const compoundedValue = p * Math.pow((1 + rate), n); // compounded value of present value
+            const annuityValue = -payments * ((Math.pow((1 + rate), n) - 1) / rate); // annuity value of annual payment (negative to represent investment)
+            return compoundedValue + annuityValue + f; // future value
+        }
+
+        function calculateDerivative(rate) {
+            const value1 = -payments * n * Math.pow((1 + rate), n - 1) / rate;
+            const value2 = -payments * ((Math.pow((1 + rate), n) - 1) / Math.pow(rate, 2)) + p * n * Math.pow((1 + rate), n - 1);
+            return value1 + value2;
+        }
+
+        let i = 0;
+        let fValue = calculateFutureValue(guess);
+        let derivative = calculateDerivative(guess);
+
+        while (Math.abs(f - fValue) > tolerance && i < 1000) {
+            guess = guess - (fValue - f) / derivative;
+            fValue = calculateFutureValue(guess);
+            derivative = calculateDerivative(guess);
+            i++;
+        }
+
+        return (guess * 100).toFixed(3); // return annual interest rate as a percentage
+
+    } else if (compundingChoice.value == "Monthly") {
+
+        const n = years * 12; // number of years
+        const p = -presentValue; // present value (negative to represent investment)
+        const r = 0.01 / 12; // starting guess for annual interest rate
+        const f = futureValue; // future value
+        let guess = r;
+        let tolerance = 0.0001;
+
+        function calculateFutureValue(rate) {
+            const compoundedValue = p * Math.pow((1 + rate), n); // compounded value of present value
+            const annuityValue = -payments * ((Math.pow((1 + rate), n) - 1) / rate); // annuity value of annual payment (negative to represent investment)
+            return compoundedValue + annuityValue + f; // future value
+        }
+
+        function calculateDerivative(rate) {
+            const value1 = -payments * n * Math.pow((1 + rate), n - 1) / rate;
+            const value2 = -payments * ((Math.pow((1 + rate), n) - 1) / Math.pow(rate, 2)) + p * n * Math.pow((1 + rate), n - 1);
+            return value1 + value2;
+        }
+
+        let i = 0;
+        let fValue = calculateFutureValue(guess);
+        let derivative = calculateDerivative(guess);
+
+        while (Math.abs(f - fValue) > tolerance && i < 1000) {
+            guess = guess - (fValue - f) / derivative;
+            fValue = calculateFutureValue(guess);
+            derivative = calculateDerivative(guess);
+            i++;
+
+            console.log("Iteration", i);
+            console.log("guess", guess);
+            console.log("fValue", fValue);
+            console.log("derivative", derivative);
+
+        }
+
+        return (guess * 100).toFixed(3); // return annual interest rate as a percentage
     }
 
-    function calculateDerivative(rate) {
-        const value1 = -annualPayment * n * Math.pow((1 + rate), n - 1) / rate;
-        const value2 = -annualPayment * ((Math.pow((1 + rate), n) - 1) / Math.pow(rate, 2)) + p * n * Math.pow((1 + rate), n - 1);
-        return value1 + value2;
-    }
 
-    let i = 0;
-    let fValue = calculateFutureValue(guess);
-    let derivative = calculateDerivative(guess);
-
-    while (Math.abs(f - fValue) > tolerance && i < 1000) {
-        guess = guess - (fValue - f) / derivative;
-        fValue = calculateFutureValue(guess);
-        derivative = calculateDerivative(guess);
-        i++;
-    }
-
-    return (guess * 100).toFixed(3); // return annual interest rate as a percentage
 }
-
-// in progress...
