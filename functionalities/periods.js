01f9e1e72ebc6f37b67c2ad7413fd606abc49061
AlekSkyHigh <aleksandar.voynov.info@gmail.com>
2023-04-11 22:48:16 +0300

    bug fixes

--- functionalities/periods.js
+++ functionalities/periods.js
@@ -1,15 +1,58 @@
+
+//CURRENTLY IN PROGRESS...
+
+// export function calculatePeriods(presentValue, annualInvestment, futureValue, interestRate) {
+
+//     let currentAmount = Number(-presentValue);
+//     let payment = Number(-annualInvestment);
+//     let finalAmount = Number(futureValue);
+//     let years = 0;
+
+//     while (currentAmount < finalAmount) {
+//         debugger
+//         currentAmount += payment;
+//         currentAmount *= Number(1 + (interestRate / 100));
+//         years++;
+//     }
+
+//     return years;
+// }
+
+
+
+
 export function calculatePeriods(presentValue, annualInvestment, futureValue, interestRate) {
 
     let currentAmount = Number(-presentValue);
     let payment = Number(-annualInvestment);
     let finalAmount = Number(futureValue);
     let years = 0;
-    
+
     while (currentAmount < finalAmount) {
+
+        let lastCurrentAmount = currentAmount;
+
         currentAmount += payment;
-        currentAmount *= (1 + (interestRate / 100));
-        years++;
+        currentAmount *= Number(1 + (interestRate / 100));
+        
+
+        if (currentAmount >= finalAmount) {
+
+            // let fractionalYears = (finalAmount - currentAmount) / payment;
+            // let fractionalPart = years = years - Math.floor(years); 0
+
+            let gornica = currentAmount - finalAmount;
+            let lastPayment = (payment - gornica);
+
+
+
+            debugger
+
+            // return (years - fractionalYears);
+        }
+
+        years++
+
     }
 
-    return years;
 }
