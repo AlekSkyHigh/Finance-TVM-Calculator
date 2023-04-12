12660cc3458772a1e2501981c2635b7152e3829d
AlekSkyHigh <aleksandar.voynov.info@gmail.com>
2023-04-12 15:38:04 +0300

    added compounding choices

--- functionalities/periods.js
+++ functionalities/periods.js
@@ -1,9 +1,44 @@
 export function calculatePeriods(presentValue, payment, featureValue, interest) {
 
-    const pv = -presentValue;
-    const pmt = -payment;
-    const rate = interest / 100;
+    const compundingChoice = document.getElementById('select-choice');
 
-    const periods = Math.log((featureValue * rate + pmt * rate + pmt) / (pmt * rate + pv * rate + pmt)) / Math.log(1 + rate);
-    return periods.toFixed(2);
+    if (compundingChoice.value == "Annualy") {
+
+        const pv = -presentValue;
+        const pmt = -payment;
+        const rate = interest / 100;
+
+        const periods = Math.log((featureValue * rate + pmt * rate + pmt) / (pmt * rate + pv * rate + pmt)) / Math.log(1 + rate);
+        return periods.toFixed(2);
+
+    } else if (compundingChoice.value == "Monthly") {
+
+        const pv = -presentValue;
+        const pmt = -payment;
+        const rate = (interest / 12) / 100;
+
+        const periods = Math.log((featureValue * rate + pmt * rate + pmt) / (pmt * rate + pv * rate + pmt)) / Math.log(1 + rate);
+        return periods.toFixed(2);
+
+    } else if (compundingChoice.value == "Semiannually") {
+
+        const pv = -presentValue;
+        const pmt = -payment;
+        const rate = (interest / 2) / 100;
+
+        const periods = Math.log((featureValue * rate + pmt * rate + pmt) / (pmt * rate + pv * rate + pmt)) / Math.log(1 + rate);
+        return periods.toFixed(2);
+
+    } else if (compundingChoice.value == "Quarterly") {
+
+        const pv = -presentValue;
+        const pmt = -payment;
+        const rate = (interest / 4) / 100;
+
+        const periods = Math.log((featureValue * rate + pmt * rate + pmt) / (pmt * rate + pv * rate + pmt)) / Math.log(1 + rate);
+        return periods.toFixed(2);
+
+    }
+
+
 }
