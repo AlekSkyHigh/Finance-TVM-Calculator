3498b2b03b0e325e004ca6ae0139ccc83b0e3791
AlekSkyHigh <aleksandar.voynov.info@gmail.com>
2023-04-12 15:30:50 +0300

    bugs fixes

--- functionalities/periods.js
+++ functionalities/periods.js
@@ -1,58 +1,9 @@
+export function calculatePeriods(presentValue, payment, featureValue, interest) {
 
-//CURRENTLY IN PROGRESS...
+    const pv = -presentValue;
+    const pmt = -payment;
+    const rate = interest / 100;
 
-// export function calculatePeriods(presentValue, annualInvestment, futureValue, interestRate) {
-
-//     let currentAmount = Number(-presentValue);
-//     let payment = Number(-annualInvestment);
-//     let finalAmount = Number(futureValue);
-//     let years = 0;
-
-//     while (currentAmount < finalAmount) {
-//         debugger
-//         currentAmount += payment;
-//         currentAmount *= Number(1 + (interestRate / 100));
-//         years++;
-//     }
-
-//     return years;
-// }
-
-
-
-
-export function calculatePeriods(presentValue, annualInvestment, futureValue, interestRate) {
-
-    let currentAmount = Number(-presentValue);
-    let payment = Number(-annualInvestment);
-    let finalAmount = Number(futureValue);
-    let years = 0;
-
-    while (currentAmount < finalAmount) {
-
-        let lastCurrentAmount = currentAmount;
-
-        currentAmount += payment;
-        currentAmount *= Number(1 + (interestRate / 100));
-        
-
-        if (currentAmount >= finalAmount) {
-
-            // let fractionalYears = (finalAmount - currentAmount) / payment;
-            // let fractionalPart = years = years - Math.floor(years); 0
-
-            let gornica = currentAmount - finalAmount;
-            let lastPayment = (payment - gornica);
-
-
-
-            debugger
-
-            // return (years - fractionalYears);
-        }
-
-        years++
-
-    }
-
+    const periods = Math.log((featureValue * rate + pmt * rate + pmt) / (pmt * rate + pv * rate + pmt)) / Math.log(1 + rate);
+    return periods.toFixed(2);
 }
